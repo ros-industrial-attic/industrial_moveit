13f693230bff6e2cdda360ed7478895c88ece9e9
Levi Armstrong <levi.armstrong@gmail.com>
2015-07-08 09:13:20 -0500

    Make ConstraintType::stringToEnum not case sensitive

--- constrained_ik/src/enum_types.cpp
+++ constrained_ik/src/enum_types.cpp
@@ -4,14 +4,15 @@
 using namespace constrained_ik::constraint_types;
 
 const std::string ConstraintType::names_[] = {"Primary", "Auxiliary", "Inactive"};
-const std::map<std::string, ConstraintTypes> ConstraintType::name_to_enum_map_ = boost::assign::map_list_of ("Primary", Primary) ("Auxiliary", Auxiliary) ("Inactive", Inactive);
+const std::map<std::string, ConstraintTypes> ConstraintType::name_to_enum_map_ = boost::assign::map_list_of ("primary", Primary) ("auxiliary", Auxiliary) ("inactive", Inactive);
 
 ConstraintTypes ConstraintType::stringToEnum(std::string constraint_type_name)
 {
   std::map<std::string, ConstraintTypes>::const_iterator it;
+  std::transform(constraint_type_name.begin(), constraint_type_name.end(), constraint_type_name.begin(), ::tolower);
   it = name_to_enum_map_.find(constraint_type_name);
   if (it != name_to_enum_map_.end())
     return it->second;
   else
-    ROS_ERROR("No enumerator named %s. Valid names: Primary, Auxiliary, Inactive", constraint_type_name.c_str());
+    ROS_ERROR("No enumerator named %s. Valid names (Not case sensitive): Primary, Auxiliary, Inactive", constraint_type_name.c_str());
 }
