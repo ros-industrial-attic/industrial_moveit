57f2dcf1535ccc8206466aa0c2a9bb71ee1ef984
Nicho, Jorge <jnicho@swri.org>
2016-12-15 15:08:12 -0600

    reinstated industrial_collision_detection_plugin.h header file (#159)

--- industrial_collision_detection/include/industrial_collision_detection/industrial_collision_detection_plugin.h
+++ industrial_collision_detection/include/industrial_collision_detection/industrial_collision_detection_plugin.h
@@ -9,29 +9,31 @@
  *
  * @copyright Copyright (c) 2016, Southwest Research Institute
  *
- * @license Software License Agreement (Apache License)\n
- * \n
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at\n
- * \n
- * http://www.apache.org/licenses/LICENSE-2.0\n
- * \n
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include <pluginlib/class_list_macros.h>
+#ifndef INDUSTRIAL_COLLISION_DETECTION_PLUGIN_H_
+#define INDUSTRIAL_COLLISION_DETECTION_PLUGIN_H_
 #include <moveit/collision_detection/collision_plugin.h>
+#include <moveit/collision_detection/collision_detector_allocator.h>
 #include <industrial_collision_detection/collision_detection/collision_robot_industrial.h>
 #include <industrial_collision_detection/collision_detection/collision_world_industrial.h>
 
+
 namespace collision_detection
 {
-  /** \brief An allocator for Industrial Moveit FCL collision detectors */
+  /** @brief An allocator for Industrial Moveit FCL collision detectors */
   class CollisionDetectorAllocatorIndustrial :
       public collision_detection::CollisionDetectorAllocatorTemplate< CollisionWorldIndustrial,
       CollisionRobotIndustrial, CollisionDetectorAllocatorIndustrial >
@@ -40,19 +42,13 @@ namespace collision_detection
     static const std::string NAME_; // defined in collision_world_industrial.cpp
   };
 
-  const std::string CollisionDetectorAllocatorIndustrial::NAME_ = "IndustrialFCL";
-
+  /**
+   * @brief The Industrial FCL based Collision Plugin
+   */
   class IndustrialFCLPluginLoader : public CollisionPlugin
   {
   public:
-    virtual bool initialize(const planning_scene::PlanningScenePtr& scene, bool exclusive) const
-    {
-      scene->setActiveCollisionDetector(CollisionDetectorAllocatorIndustrial::create(), exclusive);
-      return true;
-    }
-
+    virtual bool initialize(const planning_scene::PlanningScenePtr& scene, bool exclusive) const;
   };
-
 }
-
-PLUGINLIB_EXPORT_CLASS(collision_detection::IndustrialFCLPluginLoader, collision_detection::CollisionPlugin)
+#endif // INDUSTRIAL_COLLISION_DETECTION_PLUGIN_H_
